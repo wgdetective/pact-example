2c6a2ba64819c3ba94aa0e4ca53a389ae11dea94
Wladimir Litvinov <wl.litvinov@gmail.com>
2023-02-08 13:03:28 +0300

    update WorkProgress.adoc

--- music-grant-service-ms/build.gradle.kts
+++ music-grant-service-ms/build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.kafka:spring-kafka")
     implementation("io.projectreactor.kafka:reactor-kafka")
-    // TODO remove version
+
     implementation("org.mapstruct:mapstruct:${property("mapstructVersion")}")
 
     compileOnly("org.projectlombok:lombok")
