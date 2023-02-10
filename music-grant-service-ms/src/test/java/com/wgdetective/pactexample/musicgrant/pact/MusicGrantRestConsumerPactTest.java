87092e367fbcb8c17bb8b94e7f2057a88449f3f6
Danila Varatyntsev <danila_varatyntsev@epam.com>
2023-02-10 11:58:00 +0200

    Created rest producer test

--- music-grant-service-ms/src/test/java/com/wgdetective/pactexample/musicgrant/pact/MusicGrantRestConsumerPactTest.java
+++ music-grant-service-ms/src/test/java/com/wgdetective/pactexample/musicgrant/pact/MusicGrantRestConsumerPactTest.java
@@ -11,6 +11,9 @@ import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.PactSpecVersion;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wgdetective.pactexample.musicgrant.dto.rest.AddSongDto;
 import com.wgdetective.pactexample.musicgrant.dto.rest.SongDto;
 import com.wgdetective.pactexample.musicgrant.pact.service.AddSongTestService;
 import com.wgdetective.pactexample.musicgrant.util.TestData;
@@ -18,24 +21,42 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(PactConsumerTestExt.class)
 @ActiveProfiles("pact-test")
 class MusicGrantRestConsumerPactTest {
 
     @Pact(consumer = "User", provider = "music-grant-service-ms")
-    RequestResponsePact addSong(final PactDslWithProvider builder) {
-        return builder.uponReceiving("add song")
+    RequestResponsePact addExistingSong(final PactDslWithProvider builder) throws JsonProcessingException {
+        AddSongDto addSongDto = new AddSongDto(TestData.author, TestData.name);
+
+        return builder.given("song already exist")
+                .uponReceiving("add song")
                 .method("PUT")
                 .path("/v1/song")
-                .body("""
-                        {
-                            "author": "%s",
-                            "name": "%s"
-                        }""".formatted(TestData.author, TestData.name))
+                .headers(headers())
+                .body(new ObjectMapper().writeValueAsString(addSongDto))
+                .willRespondWith()
+                .status(409)
+                .toPact();
+    }
+
+
+    @Pact(consumer = "User", provider = "music-grant-service-ms")
+    RequestResponsePact addSong(final PactDslWithProvider builder) throws JsonProcessingException {
+        AddSongDto addSongDto = new AddSongDto(TestData.author, TestData.name);
+
+        return builder.given("no songs exist")
+                .uponReceiving("add song")
+                .method("PUT")
+                .path("/v1/song")
+                .headers(headers())
+                .body(new ObjectMapper().writeValueAsString(addSongDto))
                 .willRespondWith()
                 .status(200)
                 .headers(headers())
@@ -46,6 +67,17 @@ class MusicGrantRestConsumerPactTest {
                         }).build()
                 )
                 .toPact();
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "addExistingSong", pactVersion = PactSpecVersion.V3)
+    void addExistingSong(final MockServer mockServer) {
+
+        final RestTemplate restTemplate = new RestTemplateBuilder()
+                .rootUri(mockServer.getUrl())
+                .build();
+        assertThrows(HttpClientErrorException.class,
+                () -> new AddSongTestService(restTemplate).addSong(TestData.getAddSongDto()));
     }
 
     @Test
--- music-grant-service-ms/src/test/java/com/wgdetective/pactexample/musicgrant/pact/MusicGrantRestProviderPactTest.java
+++ music-grant-service-ms/src/test/java/com/wgdetective/pactexample/musicgrant/pact/MusicGrantRestProviderPactTest.java
@@ -7,17 +7,15 @@ import au.com.dius.pact.provider.junitsupport.Consumer;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
-import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
+import com.wgdetective.pactexample.musicgrant.exceptions.SongAlreadyExistsException;
 import com.wgdetective.pactexample.musicgrant.model.Song;
 import com.wgdetective.pactexample.musicgrant.service.SongService;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.core.publisher.Mono;
 
@@ -26,7 +24,6 @@ import java.util.List;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@ActiveProfiles("pact-test")
 @Provider("music-grant-service-ms")
 @Consumer("User")
 @PactBroker(url = "http://localhost:9292")
@@ -45,16 +42,22 @@ class MusicGrantRestProviderPactTest {
     }
 
     @TestTemplate
+//    @Disabled
     @ExtendWith(PactVerificationInvocationContextProvider.class)
     void verifyPact(final PactVerificationContext context) {
         context.getProviderInfo().setPackagesToScan(List.of("com.wgdetective.pactexample.musicgrant.pact"));
         context.verifyInteraction();
     }
 
-    @State("add song")
+    @State("no songs exist")
     void addSong() {
         when(songService.add(any(Song.class)))
                 .thenReturn(Mono.just(new Song(1L, "Rick Astley", "Never Gonna Give You Up")));
+    }
 
+    @State("song already exist")
+    void addExistingSong() {
+        when(songService.add(any(Song.class)))
+                .thenReturn(Mono.error(new SongAlreadyExistsException()));
     }
 }
